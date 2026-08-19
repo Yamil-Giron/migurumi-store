b2f16460afe87999b84d8342f5605c4a45748a7e
yamil-giron <yamilgiron98@gmail.com>
2026-08-18 21:39:48 -0400

    Agregar carpetas de app

---
+++ frontend/src/app/nucleo/disposicion/disposicion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disposicion',
+  imports: [],
+  templateUrl: './disposicion.html',
+  styleUrl: './disposicion.css',
+})
+export class Disposicion {}
---
+++ frontend/src/app/paginas/paginas-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PaginasRoutingModule } from './paginas-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PaginasRoutingModule],
+})
+export class PaginasModule {}
